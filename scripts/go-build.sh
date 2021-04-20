612a14bb5e94b4ee0eea8b9a25c03bb98a048de1
Sean Newman <sean@snewman.co>
2021-04-19 20:52:06 -0400

    Name all binaries the same thing, keep only zips

--- scripts/go-build.sh
+++ scripts/go-build.sh
@@ -12,8 +12,8 @@ goarch=${2}
 build_dir=${3}
 build_name=${4}
 package=${5}
-build_path="${build_dir}/${build_name}-${goos}-${goarch}"
 set -eoux pipefail
 
-GOOS=${goos} GOARCH=${goarch} CGO_ENABLED=0 go build -o ${build_path} ${package}
-zip -j ${build_path}.zip ${build_path}
+GOOS=${goos} GOARCH=${goarch} CGO_ENABLED=0 go build -o ${build_dir}/${build_name} ${package}
+zip -j ${build_dir}/${build_name}-${goos}-${goarch}.zip ${build_dir}/${build_name}
+rm ${build_dir}/${build_name}
